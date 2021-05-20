7dc9df9208225a80199b8e262788a32dcb1bb09f
yiiliveext <yiiliveext@gmail.com>
2021-05-20 12:58:12 +0300

    Add CORS

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Tuupola\Middleware\CorsMiddleware;
+use Yiisoft\DataResponse\Middleware\FormatDataResponseAsHtml;
 use Yiisoft\Router\Route;
 use Yiiliveext\Yii\Debug\Panel\Events\PanelEventsController;
 
 return [
     Route::get('/debug/panels/events')
+        ->middleware(FormatDataResponseAsHtml::class)
+        ->middleware(CorsMiddleware::class)
         ->action([PanelEventsController::class, 'view'])
         ->name('debug/panels/events'),
 ];
